d76a735b4fc27cc87bf20d2460bcddcafb8b6ac1
Sebastian <sebas.0070@hotmail.com>
2021-11-15 18:43:52 -0500

    h

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -7,19 +7,25 @@ import Footer from "../../components/Footer";
 
 class Users extends Component {
 
-    
-    render() {
-        
-            return (<div><Header>
-                
 
-            </Header>
-            <div className="container"></div>
+    render() {
+
+        return (<div><Header></Header>
+            <div className="container">
+                <div className="Oferta">
+                    <h1>Administración Tecnológica</h1>
+                    <h4>Duración: 10 semestres</h4>
+                    <h4>Sede: Fraternidad</h4>
+
+                    
+
+                </div>
+            </div>
             listado de ofertas
             <Footer></Footer>
-            </div>                
-            
-            )
+        </div>
+
+        )
     }
 }
 
